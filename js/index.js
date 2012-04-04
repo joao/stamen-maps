14d28d3988051dbaaccba2718a0e77dba40b255b
Joao Antunes <joao.antunes@me.com>
2012-04-05 00:04:22 +0100

    added functions for each tile and cleaned up code

--- js/index.js
+++ js/index.js
@@ -1,12 +1,60 @@
-
+var map = null;
 
 window.onload = function() {
-    var layer = new MM.StamenTileLayer("toner");
-	var map = new MM.Map("map", layer, null, [
+   watercolor();
+   /*var layer = new MM.StamenTileLayer("watercolor");
+	map = new MM.Map("map", layer, null, [
 		  new easey.DragHandler(),
           new easey.TouchHandler(),
           new easey.DoubleClickHandler(),
           new easey.MouseWheelHandler()
 	]);
 	map.setCenterZoom(new MM.Location(37.7, -122.4), 12);
+	*/
+};
+
+function toner() {
+    var layer = new MM.StamenTileLayer("toner");
+	var loc = map.getCenter();
+	var zoomLevel = map.getZoom();
+	map = new MM.Map("map", layer, null, [
+		  new easey.DragHandler(),
+          new easey.TouchHandler(),
+          new easey.DoubleClickHandler(),
+          new easey.MouseWheelHandler()
+	]);
+	map.setCenterZoom(new MM.Location(loc.lat, loc.lon), zoomLevel);
+};
+
+function terrain() {
+    var layer = new MM.StamenTileLayer("terrain");
+	var loc = map.getCenter().toFixed(2);
+	var zoomLevel = map.getZoom().toFixed(2);
+	map = new MM.Map("map", layer, null, [
+		  new easey.DragHandler(),
+          new easey.TouchHandler(),
+          new easey.DoubleClickHandler(),
+          new easey.MouseWheelHandler()
+	]);
+	map.setCenterZoom(new MM.Location(loc.lat, loc.lon), zoomLevel);
+};
+
+function watercolor() {
+    var layer = new MM.StamenTileLayer("watercolor");
+	var loc;
+	var zoomLevel;
+	if (map == null) {
+		loc = {'lat': '37.7', 'lon': '-85' };
+		zoomLevel = 4;
+	} else {
+		loc = map.getCenter();
+		zoomLevel = map.getZoom();
+	}
+	map = new MM.Map("map", layer, null, [
+		  new easey.DragHandler(),
+          new easey.TouchHandler(),
+          new easey.DoubleClickHandler(),
+          new easey.MouseWheelHandler()
+	]);
+	map.setCenterZoom(new MM.Location(loc.lat, loc.lon), zoomLevel);
 };
