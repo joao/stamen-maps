4382e4e081e2941c11af07ab90e40c71117b0f1b
Joao Antunes <joao.antunes@me.com>
2012-04-04 23:23:47 +0100

    removed extra dependencies and use Stamen JS library

--- README.md
+++ README.md
@@ -10,3 +10,5 @@ Works great on an iPad/tablets.
 ### To-Do
 —	toggle between the 3 map styles (Watercolor, Toner, Terrain)
 —	location search box with geocoding
+-   toggle between the 3 maps while maintaning location and zoom level
+—	add HTML5 geolocation, with zoom level
--- js/index.js
+++ js/index.js
@@ -1,23 +1,12 @@
-var map;
 
-var stamenTiles = 'js/stamenTiles.json';
 
 window.onload = function() {
-    var mm = com.modestmaps;
-    wax.tilejson(stamenTiles,
-        function(tj) {
-        map = new com.modestmaps.Map('map',
-            new wax.mm.connector(tj), null, [
-                new easey.DragHandler(),
-                new easey.TouchHandler(),
-                new easey.DoubleClickHandler(),
-                new easey.MouseWheelHandler()
-            ]);
-		//wax.mm.zoomer(map, tj).appendTo('controls');
-        map.setCenterZoom(new com.modestmaps.Location(25, -85), 4);
-    });
-
-    /*$('button').click(function(b) {
-        eval($('pre', this.parentNode).text());
-    });*/
+    var layer = new MM.StamenTileLayer("toner");
+	var map = new MM.Map("map", layer, null, [
+		  new easey.DragHandler(),
+          new easey.TouchHandler(),
+          new easey.DoubleClickHandler(),
+          new easey.MouseWheelHandler()
+	]);
+	map.setCenterZoom(new MM.Location(37.7, -122.4), 12);
 };
