8d874c0175651ec96e95f64bc9f40a34443cb795
Joao Antunes <joao.antunes@me.com>
2012-04-05 02:13:53 +0100

    added background and started to build the panning capabilities for geolocation

--- js/index.js
+++ js/index.js
@@ -1,6 +1,9 @@
 var map;
 var loc = {'lat': '37.7', 'lon': '-85' };
 var zoomLevel = 4;
+var gaia = {'lat': '41.1242488', 'lon': '-8.6070806'};
+var sanfrancisco = {'lat': '37', 'lon': '-120'};
+var goloc = gaia;
 
 window.onload = function() {
    drawMap('watercolor');
@@ -26,4 +29,12 @@ function swap(layer) {
 	
 };
 
+/* Pan and Zoom Map to a location */
+function go() {
+	easey.sequence(map, [{
+	     location: new MM.Location(goloc.lat, goloc.lon), zoom: 5, time: 3000, ease: 'linear'
+	   }, {
+	     location: new MM.Location(goloc.lat, goloc.lon), zoom: 14, time: 12000, ease: 'linear'
+	   }]);
+};
 
