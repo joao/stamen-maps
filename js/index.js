42192864d77630ed13018c6b693a44d912654ad5
Joao Antunes <joao.antunes@me.com>
2012-04-05 23:38:14 +0100

    initial stages of adding HTML5 geolocation

--- js/index.js
+++ js/index.js
@@ -2,8 +2,8 @@ var map;
 var loc = {'lat': '37.7', 'lon': '-85' };
 var zoomLevel = 4;
 var gaia = {'lat': '41.1242488', 'lon': '-8.6070806'};
-var sanfrancisco = {'lat': '37', 'lon': '-120'};
-var goloc = gaia;
+var sanfrancisco = {'lat': '37.7706', 'lon': '-122.3782'};
+var goloc = null;
 
 window.onload = function() {
    drawMap('watercolor');
@@ -37,4 +37,29 @@ function go() {
 	     location: new MM.Location(goloc.lat, goloc.lon), zoom: 14, time: 12000, ease: 'linear'
 	   }]);
 };
+
+/* Get the user location */
+function getLocation() {
+	var options = {
+		enableHighAccuracy: true,
+		maximumAge: 30000,
+		timeout: 5000
+	};
+	
+	if (window.navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(successCallback, errorCallback, options);
+	} else {
+		alert('The browser does not natively support geolocation.');
+	}
+}
+
+function successCallback(position){
+	goloc = {'lat': position.coords.latitude, 'lon': position.coords.longitude};
+	//alert(goloc.lat + ' ' + goloc.lon);
+	go();
+}
+
+function errorCallback(error){
+	alert('There was a problem getting the location');
+}
 
